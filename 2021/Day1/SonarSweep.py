a6adf4f92091a63723d207016d1c2d97ee8ace4c
Alex Warren <alexander.ch.warren@gmail.com>
2021-12-07 12:43:58 -0500

    Complete SonarSweep Part 2 solution

--- 2021/Day1/SonarSweep.py
+++ 2021/Day1/SonarSweep.py
@@ -1,4 +1,5 @@
 def part_one(lines):
+    print(f'Running Part 1:')
     num_increases = 0
     lastval = None
     for line in lines:
@@ -9,11 +10,25 @@ def part_one(lines):
         
         lastval = currval
     
-    print(f'Depth increased {num_increases} times\n')
+    print(f'    Depth increased {num_increases} times\n')
 
 
-def part_two():
-    pass
+def part_two(lines):
+    print(f'Running Part 2:')
+    N = len(lines)
+    last_sum = None
+    num_increases = 0
+
+    for i,(lineA,lineB,lineC) in enumerate(zip(lines[:N - 2], lines[1:N - 1], lines[2:])):
+        a,b,c = int(lineA),int(lineB),int(lineC)
+        curr_sum = a + b + c
+        # print(f'{a} + {b} + {c} = {curr_sum}')
+        if last_sum is not None and curr_sum > last_sum:
+            # print(f'{curr_sum} > {last_sum}')
+            num_increases += 1
+        last_sum = curr_sum
+    
+    print(f'    Depth increased {num_increases} times\n')
 
 
 if __name__ == '__main__':
@@ -22,5 +37,6 @@ if __name__ == '__main__':
     with open(filename, 'r') as infile:
         lines = infile.readlines()
     
-    part_one(lines)
-    # part_two()
+    # part_one(lines)
+
+    part_two(lines)
