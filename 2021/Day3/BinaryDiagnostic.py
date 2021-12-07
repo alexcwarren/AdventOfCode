7930e2d9af23766640893d091d5a4d6b71df3c77
Alex Warren <alexander.ch.warren@gmail.com>
2021-12-07 13:34:45 -0500

    Create Day3

---
+++ 2021/Day3/BinaryDiagnostic.py
@@ -0,0 +1,30 @@
+def parse(lines):
+    lines = None
+    with open(filename, 'r') as infile:
+        lines = infile.read()
+    return lines.split('\n')
+
+
+def part_one():
+    print(f'Running Part 1:')
+    
+    
+    
+    print(f'    \n')
+
+
+def part_two():
+    print(f'Running Part 2:')
+
+    
+
+    print(f'    \n')
+
+
+if __name__ == '__main__':
+    filename = 'sample.in'
+    parsed_lines = parse(filename)
+
+    part_one(parsed_lines)
+
+    # part_two(parsed_lines)
