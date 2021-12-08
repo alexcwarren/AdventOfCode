79c10aebe2f44a8aae0a028fd51e30dc397cc758
Alex Warren <alexander.ch.warren@gmail.com>
2021-12-08 15:40:54 -0500

    Change 'coordinates' to 'segments'

--- 2021/Day5/HydrothermalVenture.py
+++ 2021/Day5/HydrothermalVenture.py
@@ -5,23 +5,23 @@ def parse(lines):
     lines = data.split('\n')
     lines = [line.replace('->', '') for line in lines]
 
-    coordinates = list()
+    segments = list()
     for line in lines:
         segment = list()
         for point in line.split():
             x,y = point.split(',')
             p = (int(x), int(y))
             segment.append(p)
-        coordinates.append(tuple(segment))
+        segments.append(tuple(segment))
     
-    return coordinates
+    return segments
 
 
-def part_one(coordinates):
+def part_one(segments):
     print(f'Running Part 1:')
 
-    for coord in coordinates:
-        print(coord)
+    maxval = max([max([max(c) for c in coords]) for coords in segments])
+    print(maxval)
 
     # When using 'sample.in' data:
     # if ...:
@@ -32,7 +32,7 @@ def part_one(coordinates):
     print(f'    \n')
 
 
-def part_two(coordinates):
+def part_two(segments):
     print(f'Running Part 2:')
 
     
@@ -48,8 +48,8 @@ def part_two(coordinates):
 
 if __name__ == '__main__':
     filename = 'sample.in'
-    coordinates = parse(filename)
+    segments = parse(filename)
 
-    part_one(coordinates)
+    part_one(segments)
 
-    # part_two(coordinates)
+    # part_two(segments)
