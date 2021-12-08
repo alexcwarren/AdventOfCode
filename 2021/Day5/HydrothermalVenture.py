abf3797c9fbe27a3e6a221d33b471b45e3c7c03c
Alex Warren <alexander.ch.warren@gmail.com>
2021-12-08 15:35:19 -0500

    Redesign parse function

--- 2021/Day5/HydrothermalVenture.py
+++ 2021/Day5/HydrothermalVenture.py
@@ -5,16 +5,23 @@ def parse(lines):
     lines = data.split('\n')
     lines = [line.replace('->', '') for line in lines]
 
-    coordinates = [(line.split()[0], line.split()[1]) for line in lines]
-    print(coordinates)
+    coordinates = list()
+    for line in lines:
+        segment = list()
+        for point in line.split():
+            x,y = point.split(',')
+            p = (int(x), int(y))
+            segment.append(p)
+        coordinates.append(tuple(segment))
     
-    return []
+    return coordinates
 
 
-def part_one():
+def part_one(coordinates):
     print(f'Running Part 1:')
 
-    
+    for coord in coordinates:
+        print(coord)
 
     # When using 'sample.in' data:
     # if ...:
@@ -25,7 +32,7 @@ def part_one():
     print(f'    \n')
 
 
-def part_two():
+def part_two(coordinates):
     print(f'Running Part 2:')
 
     
@@ -41,8 +48,8 @@ def part_two():
 
 if __name__ == '__main__':
     filename = 'sample.in'
-    numbers_called, boards = parse(filename)
+    coordinates = parse(filename)
 
-    # part_one(numbers_called, boards)
+    part_one(coordinates)
 
-    part_two(numbers_called, boards)
+    # part_two(coordinates)
