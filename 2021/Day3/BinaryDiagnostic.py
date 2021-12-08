0dbf92911816ae6103ebc79df69ed797f1471371
Alex Warren <alexander.ch.warren@gmail.com>
2021-12-08 09:13:25 -0500

    Complete BinaryDiagnostic Part 2 solution

--- 2021/Day3/BinaryDiagnostic.py
+++ 2021/Day3/BinaryDiagnostic.py
@@ -44,18 +44,59 @@ def part_one(binary_nums):
     print(f'    ==> Product = {product}')
 
 
-def part_two():
+def part_two(binary_nums):
     print(f'Running Part 2:')
-
     
+    O2_rating = calculate_rating(binary_nums, 1)
+    CO2_rating = calculate_rating(binary_nums, 0)
+        
+    product = int(O2_rating, 2) * int(CO2_rating, 2)
 
-    print(f'    \n')
+    # # When using 'sample.in' data:
+    # if O2_rating == '10111' and CO2_rating == '01010' and product == 230:
+    #     print('SUCCESS')
+    # else:
+    #     print('FAILURE')
+    
+    print(f'    O2 rating = {O2_rating}, CO2 rating = {CO2_rating}')
+    print(f'    ==> Product = {product}')
+
+
+def calculate_rating(binary_nums, bit_criteria):
+    binums = binary_nums.copy()
+    bitA = str(bit_criteria)
+    bitB = str(bit_criteria ^ 1)
+
+    for i in range(len(binary_nums[0])):
+        count0 = 0
+        count1 = 0
+        majority = len(binums) // 2 + 1
+
+        for binum in binums:
+            bit = binum[i]
+            count0 += 1 if bit == '0' else 0
+            count1 += 1 if bit == '1' else 0
+
+            if count0 >= majority:
+                binums = [b for b in binums if b[i] == bitB]
+                break
+            if count1 >= majority:
+                binums = [b for b in binums if b[i] == bitA]
+                break
+        
+        if count0 == count1:
+            binums = [b for b in binums if b[i] == bitA]
+        
+        if len(binums) == 1:
+            return binums[0]
+    
+    return '0'
 
 
 if __name__ == '__main__':
     filename = 'input.in'
     parsed_lines = parse(filename)
 
-    part_one(parsed_lines)
+    # part_one(parsed_lines)
 
-    # part_two(parsed_lines)
+    part_two(parsed_lines)
