3599677d9f487cbc62bc24c46b7800bdcc0d22f9
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-13 18:57:46 -0500

    Begin Part 2

--- 2021/Day9/SmokeBasin.py
+++ 2021/Day9/SmokeBasin.py
@@ -49,14 +49,50 @@ def part_one(heightmap, using_sample=False):
     print(f'  Risk level = {risk_level}\n')
 
 
-def part_two(lines, using_sample=False):
-    pass
+def part_two(heightmap, using_sample=False):
+    print(f'Running Part 2:')
+
+    N,W,E,S = 'N','W','E','S'
+    boundary = {N:False, W:False, E:False, S:False}
+    MAX_HEIGHT = 9
+
+    basin_sizes = list()
+    basin_size = 0
+    LAST_R = len(heightmap) - 1
+    for r,row in enumerate(heightmap):
+        LAST_C = len(row) - 1
+        boundary[N] = r == 0
+        boundary[S] = r == LAST_R
+        for c,height in enumerate(row):
+            boundary[W] = c == 0
+            boundary[E] = c == LAST_C
+            #                                            EAST  SOUTH   WEST    NORTH
+            for dir,(mod_r,mod_c) in zip((E, S, W, N), ((0,1), (1,0), (0,-1), (-1,0))):
+                adj_r = r + mod_r
+                adj_c = c + mod_c
+                
+                if (0 <= adj_r <= LAST_R and 0 <= adj_c <= LAST_C
+                           and heightmap[adj_r][adj_c] == MAX_HEIGHT):
+                    boundary[dir] = True
+            if all((boundary[b] for b in boundary)):
+                # TODO
+                # Append basin_size
+                basin_sizes.append(basin_size)
+                # Reset basin_size
+                basin_size = 0
+                # Reset boundaries
+                
+
+    if using_sample:
+        verify_sample()
+    
+    print(f'  \n')
 
 
 if __name__ == '__main__':
     filename = 'input.in'
     heightmap = parse(filename)
 
-    part_one(heightmap, filename == 'sample.in')
+    # part_one(heightmap, filename == 'sample.in')
 
-    # part_two(lines, filename == 'sample.in')
+    part_two(heightmap, filename == 'sample.in')
