b4b2ae84ee9dba91d2dc4eecc63be69b5eb41333
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-14 08:41:48 -0500

    Add Part 2 solution

--- 2021/Day10/SyntaxScoring.py
+++ 2021/Day10/SyntaxScoring.py
@@ -27,23 +27,81 @@ def verify_sample(actual_vals, expected_vals):
 def part_one(lines, using_sample=False):
     print(f'Running Part 1:')
     
-    print(lines)
+    char_scores = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137
+    }
+    total_score = 0
 
-    # TODO
-    # if using_sample:
-    #     verify_sample()
+    type_stack = list()
+    for line in lines:
+        for chr in line:
+            if chr in chars:
+                type_stack.append(chars[chr])
+            elif chr != type_stack.pop():
+                total_score += char_scores[chr]
+                break
+
+    if using_sample:
+        verify_sample(total_score, 26397)
     
-    print(f'  \n')
+    print(f'  Total score = {total_score}\n')
 
 
 def part_two(lines, using_sample=False):
-    pass
+    print(f'Running Part 2:')
+
+    char_scores = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+    line_scores = list()
+
+    for line in lines.copy():
+        type_stack = list()
+        is_corrupted = False
+        for chr in line:
+            if chr in chars:
+                type_stack.append(chars[chr])
+            elif chr != type_stack.pop():
+                lines.remove(line)
+                is_corrupted = True
+                break
+        if is_corrupted:
+            continue
+
+        total_score = 0
+        for chr in reversed(type_stack):
+            total_score *= 5
+            total_score += char_scores[chr]
+        line_scores.append(total_score)
+    
+    line_scores.sort()
+    mid = len(line_scores) // 2
+    middle_score = line_scores[mid]
+
+
+    if using_sample:
+        verify_sample(middle_score, 288957)
+    
+    print(f'  Middle score = {middle_score}\n')
 
 
 if __name__ == '__main__':
-    filename = 'sample.in'
+    filename = 'input.in'
     lines = parse(filename)
+
+    chars = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+        '<': '>'
+    }
 
     part_one(lines, filename == 'sample.in')
 
-    # part_two(lines, filename == 'sample.in')
+    part_two(lines, filename == 'sample.in')
