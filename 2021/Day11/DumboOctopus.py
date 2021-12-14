1ee9bb040391f25303e23d5d4dfb0ab14ff88ae8
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-14 08:46:11 -0500

    Create Day11/DumboOctopus

---
+++ 2021/Day11/DumboOctopus.py
@@ -0,0 +1,49 @@
+def parse(filename):
+    data = None
+    with open(filename, 'r') as infile:
+        data = infile.read().strip()
+    return [[int(d) for d in datum] for datum in data.split('\n')]
+
+
+def verify_sample(actual_vals, expected_vals):
+    if isinstance(actual_vals, list):
+        if len(actual_vals) != len(expected_vals):
+            print('ERROR: Count of actual values != count of expected values:', end=' ')
+            print(f'len(actual_vals)={len(actual_vals)}, len(expected_vals)={len(expected_vals)}')
+            return False
+    else:
+        actual_vals = [actual_vals]
+        expected_vals = [expected_vals]
+    
+    for a,e in zip(actual_vals, expected_vals):
+        if a != e:
+            print(f'FAILED: Expected {e} got {a}')
+            return False
+    
+    print('SUCCESS')
+    return True
+
+
+def part_one(octopuses, using_sample=False):
+    print(f'Running Part 1:')
+    
+    print(octopuses)
+
+    # TODO
+    # if using_sample:
+    #     verify_sample()
+    
+    print(f'  \n')
+
+
+def part_two(lines, using_sample=False):
+    pass
+
+
+if __name__ == '__main__':
+    filename = 'sample.in'
+    octopuses = parse(filename)
+
+    part_one(octopuses, filename == 'sample.in')
+
+    # part_two(lines, filename == 'sample.in')
