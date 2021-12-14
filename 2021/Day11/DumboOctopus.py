11a92b728cc513b28c82c773758cb00b01849527
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-14 14:04:19 -0500

    Begin Part 1 solution

--- 2021/Day11/DumboOctopus.py
+++ 2021/Day11/DumboOctopus.py
@@ -24,16 +24,84 @@ def verify_sample(actual_vals, expected_vals):
     return True
 
 
-def part_one(octopuses, using_sample=False):
+def part_one(octopus_levels, using_sample=False):
     print(f'Running Part 1:')
     
-    print(octopuses)
+    MAX_ROW = len(octopus_levels) - 1
+    MAX_COL = len(octopus_levels[MAX_ROW]) - 1
+    octopuses = list()
 
-    # TODO
-    # if using_sample:
-    #     verify_sample()
+    class Octopus:
+        FLASH_THRESHOLD = 9
+
+        def __init__(self, level, coordinates):
+            self.level = level
+            self.flashed = False
+            self.row, self.col = coordinates
+            self.adjacents = self.__determine_adjacents()
+        
+        def __determine_adjacents(self):
+            adjacents = list()
+            # f g h
+            # e x a
+            # d c b               a      b      c       d       e       f        g       h
+            for mod_r,mod_c in ((0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1)):
+                adj_r = self.row + mod_r
+                adj_c = self.col + mod_c
+                if 0 <= adj_r <= MAX_ROW and 0 <= adj_c <= MAX_COL:
+                    adjacents.append((adj_r,adj_c))
+            return tuple((r,c) for r,c in adjacents)
+        
+        def step(self):
+            self.level += 1
+
+            if self.level > Octopus.FLASH_THRESHOLD:
+                return self.flash()
+            return 0
+        
+        def flash(self):
+            if self.flashed:
+                return 0
+            
+            self.flashed = True
+            self.level = 0
+
+            num_adj_flashes = 1
+            for r,c in self.adjacents:
+                num_adj_flashes += octopuses[r][c].step()
+            return num_adj_flashes
+
+        def reset(self):
+            self.flashed = False
+        
+        def __str__(self):
+            return f'{self.level}'
+        
+        def __repr__(self):
+            string =  f'Octopus[{self.row}][{self.col}]:\n'
+            string += f'  level={self.level} flashed={self.flashed}\n'
+            string += f'  adjacents={self.adjacents}\n'
+            return string
     
-    print(f'  \n')
+    octopuses = [[Octopus(level, (r,c)) for c,level in enumerate(row)] for r,row in enumerate(octopus_levels)]
+    # for row in octopuses:
+    #     for octo in row:
+    #         print(repr(octo))
+
+    num_flashes = 0
+    for step in range(10):
+        for row in octopuses:
+            for octo in row:
+                num_flashes += octo.step()
+                octo.reset()
+                print(octo, end='')
+            print()
+        print()
+
+    if using_sample:
+        verify_sample(num_flashes, 1656)
+    
+    print(f'  Number of flashes = {num_flashes}\n')
 
 
 def part_two(lines, using_sample=False):
