313c21076e8e3f68959c48ee5524c2813eb6a331
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-15 08:46:53 -0500

    Begin Part 2 solution

--- 2021/Day14/ExtendedPolymerization.py
+++ 2021/Day14/ExtendedPolymerization.py
@@ -53,14 +53,37 @@ def part_one(template, pairs, using_sample=False):
     print(f'  Most common - Least common = {diff}\n')
 
 
-def part_two(lines, using_sample=False):
-    pass
+def part_two(template, pairs, using_sample=False):
+    print(f'Running Part 2:')
+    
+    elem_set = set(pairs.values())
+    elem_counts = {elem:template.count(elem) for elem in elem_set}
+    
+    total_steps = 10
+    # total_steps = 40
+    for __ in range(total_steps):
+        new_template = ''
+        for i,(elem1,elem2) in enumerate(zip(template[:-1], template[1:])):
+            result = pairs[elem1 + elem2]
+            new_template += f'{elem1}{result}'
+            if i >= len(template) - 2:
+                new_template += f'{elem2}'
+            elem_counts[result] += 1
+        template = new_template
+    
+    diff = max(count for count in elem_counts.values()) - min(count for count in elem_counts.values())
+
+    if using_sample:
+        verify_sample(diff, 1588)
+        # verify_sample(diff, 2188189693529)
+    
+    print(f'  Most common - Least common = {diff}\n')
 
 
 if __name__ == '__main__':
-    filename = 'input.in'
+    filename = 'sample.in'
     template, pairs = parse(filename)
 
-    part_one(template, pairs, filename == 'sample.in')
+    # part_one(template, pairs, filename == 'sample.in')
 
-    # part_two(lines, filename == 'sample.in')
+    part_two(template, pairs, filename == 'sample.in')
