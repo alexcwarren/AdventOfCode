f1946991ad48bda722c1ec585b4916759acf6090
Warren (US), Alexander C <alexander.c.warren@boeing.com>
2021-12-17 18:05:09 -0500

    Add Part 2 solution

--- 2021/Day16/PacketDecoder.py
+++ 2021/Day16/PacketDecoder.py
@@ -1,4 +1,5 @@
 from math import prod
+from typing import List
 
 
 def parse(filename):
@@ -48,6 +49,13 @@ class BITS_Packet:
         'less_than': (lambda x : 1 if x[0] < x[1] else 0),
         'equal_to': (lambda x : 1 if x[0] == x[1] else 0)
     }
+
+    class Literal:
+        def __init__(self, num):
+            self.num = num
+        
+        def evaluate(self):
+            return self.num
 
     def __init__(self, packet_str, is_hex=False):
         self.hex = None
@@ -112,7 +120,7 @@ class BITS_Packet:
             bin_literal += self.extract_bits(4)
             if first_bit == '0':
                 break
-        return int(bin_literal, 2)
+        return BITS_Packet.Literal(int(bin_literal, 2))
     
     def parse_subpacket(self):
         sub_packet = BITS_Packet(self.extract_bits())
@@ -127,23 +135,18 @@ class BITS_Packet:
         return version_sum
     
     def evaluate(self):
-        print(self)
-        return 0
-        # operands = list()
-        # print('evaluating...')
-        # print(repr(self))
-        # for c in self.contents:
-        #     if isinstance(c, BITS_Packet):
-        #         if c.operationID == 'literal':
-        #             return c.contents
-        #             # operands.extend(('literal', c.contents))
-        #         else:
-        #             operands.append(c.evaluate())
-        #     else:
-        #         return c
-        #         # operands.append(c)
-        # print(operands)
-        # return (self.operationID, operands)
+        operands = list()
+        for c in self.contents:
+            result = c.evaluate()
+            if isinstance(result, List):
+                operands.extend(result)
+            else:
+                operands.append(result)
+        
+        if self.operationID != 'literal':
+            result = self.operations[self.operationID](operands)
+            return result
+        return operands
     
     def __repr__(self):
         string = ''
@@ -204,29 +207,31 @@ def part_one(packets, using_sample=False):
 
 def part_two(lines, using_sample=False):
     expected_results = [
-        6,  # D2FE28
-        9,  # 38006F45291200
-        14, # EE00D40C823060
-        16, # 8A004A801A8002F478
-        12, # 620080001611562C8802118E34
-        23, # C0015000016115A2E0802F182340
-        31  # A0016C880162017C3686B18A3D4780
+        3,  # C200B40A82
+        54, # 04005AC33890
+        7,  # 880086C3E88112
+        9,  # CE00C43D881120
+        1,  # D8005AC2A8F0
+        0,  # F600BC2D8F
+        0,  # 9C005AC2F8F0
+        1   # 9C0141080250320F1802104A08
     ]
     
     for i,hex_packet in enumerate(packets):
         packet = BITS_Packet(hex_packet, is_hex=True)
         result = packet.evaluate()
         
-        # if using_sample:
-        #     verify_sample(result, expected_results[i])
+        if using_sample:
+            verify_sample(result, expected_results[i])
         
         print(f'  {hex_packet} result = {result}\n')
 
 
 if __name__ == '__main__':
-    filename = 'sample.in'
+    filename = 'input.in'
     packets = parse(filename)
+    part_one(packets, filename == 'sample.in')
 
-    # part_one(packets, filename == 'sample.in')
-
-    part_two(packets, filename == 'sample.in')
+    # filename = 'sample.in'
+    # packets = parse(filename)
+    part_two(packets, filename == 'sample2.in')
