e54938a7530e94246aac0bf549692480407118f3
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-02 12:57:34 -0500

    Move functionality inside class

--- 2022/Day 1/calorie_counting.py
+++ 2022/Day 1/calorie_counting.py
@@ -2,32 +2,35 @@ from argparse import ArgumentParser
 from os import path
 
 
-def find_most_calories(file_path: str):
-    calories_sum: int = 0
-    max_sum: int = 0
+class CalorieCounter:
+    def __init__(self, prog_name: str):
+        parser = ArgumentParser(
+            prog=prog_name,
+            usage=f"python {prog_name} <filepath>",
+        )
+        parser.add_argument("filepath")
+        args = parser.parse_args()
 
-    with open(file_path, "r") as file:
-        for line in file:
-            if line.strip().isdigit():
-                calories_sum += int(line)
-            else:
-                max_sum = max(calories_sum, max_sum)
-                calories_sum = 0
+        if not path.isfile(args.filepath):
+            print(f"ERROR: {args.filepath} does not exist")
+        else:
+            self.__filepath: str = args.filepath
 
-    return max_sum
+    def find_most_calories(self) -> int:
+        calories_sum: int = 0
+        max_sum: int = 0
+
+        with open(self.__filepath, "r") as file:
+            for line in file:
+                if line.strip().isdigit():
+                    calories_sum += int(line)
+                else:
+                    max_sum = max(calories_sum, max_sum)
+                    calories_sum = 0
+
+        return max_sum
 
 
 if __name__ == "__main__":
-    prog_name: str = "calorie_counting.py"
-    parser = ArgumentParser(
-        prog=prog_name,
-        description="",
-        usage=f"python {prog_name} <filepath>",
-    )
-    parser.add_argument("filepath")
-    args = parser.parse_args()
-
-    if not path.isfile(args.filepath):
-        print(f"ERROR: {args.filepath} does not exist")
-    else:
-        print(find_most_calories(args.filepath))
+    result: int = CalorieCounter("calorie_counting.py").find_most_calories()
+    print(result)
