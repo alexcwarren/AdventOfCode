f14da898c0a5aa1f87d0a8100ecd859616a53858
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-03 16:59:18 -0500

    Add test_sample_input()

---
+++ 2022/Day 1/test_calorie_counting.py
@@ -0,0 +1,12 @@
+import pytest
+
+import calorie_counting
+
+
+@pytest.fixture
+def script():
+    return calorie_counting.CalorieCounter()
+
+
+def test_sample_input(script):
+    assert script.find_most_calories() == 24000
