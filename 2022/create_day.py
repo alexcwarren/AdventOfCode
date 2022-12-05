8ac07635dab1ecc2afb5b21d9f8bdf43ae7dc972
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-05 00:21:35 -0500

    Create create_day.py

---
+++ 2022/create_day.py
@@ -0,0 +1,22 @@
+# command-line argument (1):
+# URL to problem
+
+# {1} = day_number
+
+# {2} = problem_title
+
+#################################################
+
+# make new directory: "Day {1}"
+
+# copy template Python file: "{2}.py"
+
+# copy template Markdown file: "Outline.py"
+
+# input formatted problem description into "Outline.py"
+
+# create empty "sample.in" file
+
+# download "input.in" file via URL
+
+# copy template Python test file: "test_{2}.py"
