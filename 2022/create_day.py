9d8e34d8effda1046e4ade9158784b7d7cfa23ed
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-05 01:24:58 -0500

    Add beginning functionality

--- 2022/create_day.py
+++ 2022/create_day.py
@@ -20,3 +20,77 @@
 # download "input.in" file via URL
 
 # copy template Python test file: "test_{2}.py"
+
+
+from argparse import ArgumentParser
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+class DayCreator:
+    TEMPLATE_PATH: str = "./templates/"
+
+    def __init__(self):
+        self.process_arguments()
+
+    def create_new_day(self):
+        self.get_url_data()
+        self.create_directory()
+        self.create_files()
+
+    def process_arguments(self):
+        parser = ArgumentParser(
+            prog="create_day.py",
+            usage="python create_day.py <AdventOfCode.com URL>"
+        )
+        parser.add_argument("url")
+        args = parser.parse_args()
+        self.url: str = str(args.url).lower()
+        if "adventofcode.com" not in self.url:
+            parser.error(f"{self.url}: Please provide \"adventofcode.com\" URL.")
+
+    def get_url_data(self):
+        response = requests.get(self.url)
+        self.html_content = BeautifulSoup(response.content, "html.parser")
+        self.day_number, self.problem_title = self.get_problem_header()
+
+    def get_problem_header(self) -> tuple[str]:
+        problem_header: str =  self.html_content.find("h2").text.strip()
+        day_match = re.search(r"\d", problem_header)
+        title_match = re.search(r": (.+) -", problem_header)
+        title: str = self.convert_to_filename(title_match.group(1))
+        return (day_match.group(), title)
+
+    def convert_to_filename(self, text: str) -> str:
+        return text.lower().replace(" ", "_")
+
+    def create_directory(self):
+        pass
+
+    def create_files(self):
+        self.create_python_file()
+        self.create_markdown_file()
+        self.create_sample_data_file()
+        self.create_input_data_file()
+        self.create_python_test_file()
+
+    def create_python_file(self):
+        pass
+    
+    def create_markdown_file(self):
+        pass
+    
+    def create_sample_data_file(self):
+        pass
+    
+    def create_input_data_file(self):
+        pass
+    
+    def create_python_test_file(self):
+        pass
+
+
+if __name__ == "__main__":
+    script = DayCreator()
+    script.create_new_day()
