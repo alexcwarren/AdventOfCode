447b89ffce3ca465612f4dd6a3482c74f703cb1c
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-06 15:45:27 -0500

    Properly format create_day.py

--- create_day.py
+++ create_day.py
@@ -1,34 +1,11 @@
-# command-line argument (1):
-# URL to problem
-
-# {1} = day_number
-
-# {2} = problem_title
-
-#################################################
-
-# make new directory: "Day {1}"
-
-# copy template Python file: "{2}.py"
-
-# copy template Markdown file: "Outline.py"
-
-# input formatted problem description into "Outline.py"
-
-# create empty "sample.in" file
-
-# download "input.in" file via URL
-
-# copy template Python test file: "test_{2}.py"
-
-
 from argparse import ArgumentParser
-from requests import get as get_response
-from bs4 import BeautifulSoup
-from re import search, match, sub, findall
 from os import getcwd, mkdir, path
+from re import findall, match, search, sub
 from shutil import copy
+
+from bs4 import BeautifulSoup
 from markdownify import markdownify
+from requests import get as get_response
 
 
 class DayCreator:
@@ -44,27 +21,28 @@ class DayCreator:
 
     def create_new_day(self):
         self.get_url_data()
-        # self.check_year_directory()
-        # self.create_day_directory()
+        self.check_year_directory()
+        self.create_day_directory()
         self.create_files()
 
     def process_arguments(self):
         parser = ArgumentParser(
-            prog="create_day.py",
-            usage="python create_day.py <AdventOfCode.com URL>"
+            prog="create_day.py", usage="python create_day.py <AdventOfCode.com URL>"
         )
         parser.add_argument("url")
         args = parser.parse_args()
         self.url: str = str(args.url).lower()
         if "adventofcode.com" not in self.url:
-            parser.error(f"{self.url}: Please provide \"adventofcode.com\" URL.")
+            parser.error(f'{self.url}: Please provide "adventofcode.com" URL.')
 
     def get_url_data(self):
         response = get_response(self.url)
         self.html_content = BeautifulSoup(response.content, "html.parser")
         self.year: str = self.get_year()
         self.day_number, self.problem_name = self.get_problem_header()
-        self.class_name: str = self.problem_name.replace("_", " ").title().replace(" ", "")
+        self.class_name: str = (
+            self.problem_name.replace("_", " ").title().replace(" ", "")
+        )
         self.problem_title, self.problem_description = self.get_problem_description()
 
     def get_year(self) -> str:
@@ -73,7 +51,7 @@ class DayCreator:
         return year.group(1)
 
     def get_problem_header(self) -> tuple[str]:
-        problem_header: str =  self.html_content.find("h2").text.strip()
+        problem_header: str = self.html_content.find("h2").text.strip()
         day_match = search(r"\d", problem_header)
         title_match = search(r": (.+) -", problem_header)
         title: str = self.convert_to_filename(title_match.group(1))
@@ -96,14 +74,11 @@ class DayCreator:
 
             elif "href" in element_str:
                 element_str = sub(
-                    r"href=\"(.+)\"", f"href=\"{self.url}\\g<1>\"", element_str
+                    r"href=\"(.+)\"", f'href="{self.url}\\g<1>"', element_str
                 )
 
             element_markdown = markdownify(
-                element_str,
-                heading_style="ATX",
-                bullets="-",
-                code_language="python"
+                element_str, heading_style="ATX", bullets="-", code_language="python"
             ).strip()
 
             em_code_elements: list = findall(r"`\*\d+\*`", element_markdown)
@@ -119,7 +94,10 @@ class DayCreator:
             else:
                 description += f"{element_markdown}\n\n"
 
-        return (title, description.strip().replace("*", "**").replace("\n```\n", "```\n"))
+        return (
+            title,
+            description.strip().replace("*", "**").replace("\n```\n", "```\n"),
+        )
 
     def convert_to_filename(self, text: str) -> str:
         return text.lower().replace(" ", "_")
@@ -133,38 +111,40 @@ class DayCreator:
         self.day_dir: str = f"{self.year_dir}/Day {self.day_number}"
         try:
             mkdir(self.day_dir)
-        except:
-            print(
-                f"ERROR: directory \"{self.day_dir}\" already exists."
-            )
+        except Exception as exception:
+            print(exception)
             exit()
 
     def create_files(self):
-        # self.create_python_file()
-        # self.create_markdown_file()
-        # self.create_sample_data_file()
-        # self.create_input_data_file()
+        self.create_python_file()
+        self.create_markdown_file()
+        self.create_sample_data_file()
+        self.create_input_data_file()
         self.create_python_test_file()
 
     def create_python_file(self):
-        # python_file: str = f"{self.day_dir}/{self.problem_name}.py"
-        python_file: str = f"{self.current_dir}/test.py"
-        copy(
-            f"{self.templates_dir}/template.py",
-            python_file
-        )
+        python_file: str = f"{self.day_dir}/{self.problem_name}.py"
+        copy(f"{self.templates_dir}/template.py", python_file)
 
         file_contents: str = self.get_file_contents(python_file)
-        file_contents = file_contents.replace(self.REPLACEMENTS.CLASS_NAME, self.class_name).replace(self.REPLACEMENTS.PROBLEM_NAME, self.problem_name).replace(self.REPLACEMENTS.DAY_NUMBER, self.day_number)
+        file_contents = (
+            file_contents.replace(self.REPLACEMENTS.CLASS_NAME, self.class_name)
+            .replace(self.REPLACEMENTS.PROBLEM_NAME, self.problem_name)
+            .replace(self.REPLACEMENTS.DAY_NUMBER, self.day_number)
+        )
 
         self.write_file_contents(file_contents, python_file)
-    
+
     def create_markdown_file(self):
         outline_file: str = f"{self.day_dir}/Outline.md"
         copy(f"{self.templates_dir}/Outline.md", outline_file)
 
         file_contents: str = self.get_file_contents(outline_file)
-        file_contents = file_contents.replace("REPLACE_WITH_TITLE", self.problem_title).replace("REPLACE_WITH_URL", self.url).replace("REPLACE_WITH_DESCRIPTION", self.problem_description)
+        file_contents = (
+            file_contents.replace("REPLACE_WITH_TITLE", self.problem_title)
+            .replace("REPLACE_WITH_URL", self.url)
+            .replace("REPLACE_WITH_DESCRIPTION", self.problem_description)
+        )
 
         self.write_file_contents(file_contents, outline_file)
 
@@ -177,25 +157,24 @@ class DayCreator:
     def write_file_contents(self, file_contents: str, filepath: str):
         with open(filepath, "w") as write_file:
             write_file.write(file_contents)
-    
+
     def create_sample_data_file(self):
         self.create_empty_file("sample.in")
 
     def create_empty_file(self, filename: str):
         self.write_file_contents("", f"{self.day_dir}/{filename}")
-    
+
     def create_input_data_file(self):
         self.create_empty_file("input.in")
-    
+
     def create_python_test_file(self):
         test_file: str = f"{self.day_dir}/test_{self.problem_name}.py"
-        copy(
-            f"{self.templates_dir}/test.py",
-            test_file
-        )
+        copy(f"{self.templates_dir}/test.py", test_file)
 
         file_contents: str = self.get_file_contents(test_file)
-        file_contents = file_contents.replace(self.REPLACEMENTS.PROBLEM_NAME, self.problem_name).replace(self.REPLACEMENTS.CLASS_NAME, self.class_name)
+        file_contents = file_contents.replace(
+            self.REPLACEMENTS.PROBLEM_NAME, self.problem_name
+        ).replace(self.REPLACEMENTS.CLASS_NAME, self.class_name)
 
         self.write_file_contents(file_contents, test_file)
 
