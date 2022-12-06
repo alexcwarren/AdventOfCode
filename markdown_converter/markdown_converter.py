0432009001b2734fdbd70be6725f440eb895d6ca
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-06 16:57:00 -0500

    Add get_markdown_element()

--- markdown_converter/markdown_converter.py
+++ markdown_converter/markdown_converter.py
@@ -10,7 +10,7 @@ class MarkdownConverter:
         self.intput_file: str = f"{getcwd()}/html.txt"
         self.output_file: str = f"{getcwd()}/markdown.md"
 
-    def get_markdown(self, html):
+    def get_markdown(self, html) -> str:
         markdown: str = ""
         for element in html:
             element_str = str(element)
@@ -42,6 +42,34 @@ class MarkdownConverter:
             )
 
         return markdown.strip()
+
+    def get_markdown_element(self, element) -> str:
+        element_str = str(element)
+        if match(r"\s+", element_str):
+            return ""
+
+        if "h2" in element_str:
+            element_str = sub(r"<(/?)h2.*?>", r"<\g<1>h1>", element_str)
+            element_str = element_str.replace("<>", "").replace("</>", "")
+
+        elif "href" in element_str:
+            element_str = sub(r"href=\"(.+)\"", f'href="{self.url}\\g<1>"', element_str)
+
+        markdown_element = markdownify(
+            element_str, heading_style="ATX", bullets="-", code_language="python"
+        ).strip()
+
+        em_code_elements: list = findall(r"`\*\d+\*`", markdown_element)
+        if em_code_elements:
+            for element in em_code_elements:
+                digits_search: str = search(r"\d+", element)
+                markdown_element = markdown_element.replace(
+                    element, f"*`{digits_search.group()}`*"
+                )
+        markdown_element = f"{markdown_element}\n\n".replace("*", "**").replace(
+            "\n```\n", "```\n"
+        )
+        return markdown_element
 
     def write_markdown(self):
         html: str = None
