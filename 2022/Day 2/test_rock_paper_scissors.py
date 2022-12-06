c8df810d6548555ad2d55804d5210e9adbb234c7
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-06 15:48:55 -0500

    Create "Day 2"

---
+++ 2022/Day 2/test_rock_paper_scissors.py
@@ -0,0 +1,16 @@
+import pytest
+
+import rock_paper_scissors
+
+
+@pytest.fixture
+def script():
+    return rock_paper_scissors.RockPaperScissors()
+
+
+def test_sample_input(script):
+    pass
+
+
+def test_actual_input(script):
+    pass
