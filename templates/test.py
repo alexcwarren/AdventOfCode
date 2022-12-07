14cd61f3106cd0488c617f8dee67604a08cac22f
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-07 12:13:12 -0500

    Add "sample.in" argument

--- templates/test.py
+++ templates/test.py
@@ -5,7 +5,7 @@ import REPLACE_WITH_PROBLEM_NAME
 
 @pytest.fixture
 def script():
-    return REPLACE_WITH_PROBLEM_NAME.REPLACE_WITH_CLASS_NAME()
+    return REPLACE_WITH_PROBLEM_NAME.REPLACE_WITH_CLASS_NAME("sample.in")
 
 
 def test_sample_input(script):
