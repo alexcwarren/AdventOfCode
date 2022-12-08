3841d76ee8b61d6bea8d6e2829f7fde8e6c18916
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-08 15:47:31 -0500

    Solve Part 1

--- src/puzzles/2022/Day 3/rucksack_reorganization.py
+++ src/puzzles/2022/Day 3/rucksack_reorganization.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from os import path
+from string import ascii_letters
 
 
 class RucksackReorganization:
@@ -30,12 +31,31 @@ class RucksackReorganization:
 
     def print_result(self):
         if self.is_part1:
-            print(f"{self.sum_shared_item_priorities()}")
+            print(
+                f"Sum of priorities of shared items is {self.sum_shared_item_priorities()}"
+            )
         else:
             print(f"{self.solve_part2()}")
 
-    def sum_shared_item_priorities(self):
-        pass
+    def sum_shared_item_priorities(self) -> int:
+        prioritiy_numbers: dict[str, int] = {
+            letter: number for number, letter in enumerate(ascii_letters, 1)
+        }
+        shared_item_sum: int = 0
+        with open(self.__filepath, "r") as read_file:
+            for rucksack_contents in read_file:
+                shared_item: str = self.find_shared_item(rucksack_contents.strip())
+                shared_item_sum += prioritiy_numbers[shared_item]
+        return shared_item_sum
+
+    def find_shared_item(self, contents: str) -> str:
+        num_items: int = len(contents)
+        fst_half: str = contents[:num_items // 2]
+        snd_half: str = contents[num_items // 2:]
+        for item in fst_half:
+            if item in snd_half:
+                return item
+        return None
 
     def solve_part2(self):
         pass
