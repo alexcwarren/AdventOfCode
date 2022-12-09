d8cc2344df5c1be245c8ae68ee2ac945c4f7a93e
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-09 14:25:03 -0500

    Update test function names

--- src/create_day/templates/test.py
+++ src/create_day/templates/test.py
@@ -8,9 +8,9 @@ def script():
     return REPLACE_WITH_PROBLEM_NAME.REPLACE_WITH_CLASS_NAME("sample.in")
 
 
-def test_sample_input(script):
+def test_sample_input_part1(script):
     pass
 
 
-def test_actual_input(script):
+def test_sample_input_part2(script):
     pass
