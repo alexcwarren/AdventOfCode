998545d7cfb7e085629e15c04cc87469bdc189f9
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-09 14:26:19 -0500

    Update test function names

--- src/puzzles/2022/Day 3/test_rucksack_reorganization.py
+++ src/puzzles/2022/Day 3/test_rucksack_reorganization.py
@@ -8,9 +8,9 @@ def script():
     return rucksack_reorganization.RucksackReorganization("sample.in")
 
 
-def test_sample_input(script):
+def test_sample_input_part1(script):
     assert script.sum_shared_item_priorities() == 157
 
 
-def test_actual_input(script):
+def test_sample_input_part2(script):
     assert script.sum_group_type_item_priorities() == 70
