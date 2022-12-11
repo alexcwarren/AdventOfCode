5aa787a0fcf00b71c90c29fe4ead3e65e9b6abde
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-11 16:33:23 -0500

    Solve 2022 Day 4 Part 1

--- src/puzzles/2022/Day 4/camp_cleanup.py
+++ src/puzzles/2022/Day 4/camp_cleanup.py
@@ -30,28 +30,32 @@ class CampCleanup:
 
     def print_result(self):
         if self.is_part1:
-            print(f"Number of ranges contained by another = {self.get_num_contained_ranges()}")
+            print(
+                f"Number of ranges contained by another = {self.get_num_contained_ranges()}"
+            )
         else:
             print(f"{self.solve_part2()}")
 
     def get_num_contained_ranges(self) -> int:
-        num_contained_ranges_count: int = 0
+        num_contained_ranges: int = 0
         with open(self.__filepath, "r") as read_file:
             for pair in read_file:
                 range1_str, range2_str = pair.strip().split(",")
 
-                range1_id_a, range1_id_b = range1_str.split("-")
+                range1_id_a, range1_id_b = self.get_range_parameters(range1_str)
                 range1 = range(range1_id_a, range1_id_b + 1)
 
-                range2_id_a, range2_id_b = range2_str.split("-")
+                range2_id_a, range2_id_b = self.get_range_parameters(range2_str)
                 range2 = range(range2_id_a, range2_id_b + 1)
 
-                if all(id in range2 for id in range1):
-                    num_contained_ranges_count += 1
-        return num_contained_ranges_count
+                if all(id in range2 for id in range1) or all(
+                    id in range1 for id in range2
+                ):
+                    num_contained_ranges += 1
+        return num_contained_ranges
 
-
-
+    def get_range_parameters(self, range_str) -> tuple:
+        return (int(num) for num in range_str.split("-"))
 
     def solve_part2(self):
         pass
