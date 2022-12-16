0645f32ef5f3448835c9810b3587d9af249bec28
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-15 23:24:35 -0500

    y2022d7p1: Begin implementing

--- src/puzzles/2022/Day 8/treetop_tree_house.py
+++ src/puzzles/2022/Day 8/treetop_tree_house.py
@@ -35,7 +35,54 @@ class TreetopTreeHouse:
             print(f"{self.solve_part2()}")
 
     def solve_part1(self):
-        pass
+        num_visible_trees: int = 0
+        with open(self.__filepath, "r") as read_file:
+            columns: list = None
+            for line in (line.strip() for line in read_file):
+                if columns is None:
+                    columns = [""] * len(line)
+
+                num_visible_trees += self.count_visible_trees(
+                    [int(tree) for tree in line]
+                )
+
+                for i, char in enumerate(line):
+                    columns[i] += char
+            for col in columns:
+                num_visible_trees += self.count_visible_trees(
+                    [int(tree) for tree in col]
+                )
+        return num_visible_trees
+
+    def count_visible_trees(self, tree_line: list) -> int:
+        tallest_tree: int = max(tree_line)
+        visible_trees_a: set = self.get_visible_tree_indices_forward(tree_line[:-1], tallest_tree)
+        visible_trees_b: set = self.get_visible_tree_indices_backward(tree_line[1:], tallest_tree)
+        return len(visible_trees_a.union(visible_trees_b))
+
+    def get_visible_tree_indices_forward(self, tree_line: list, tallest_tree: int = 9) -> set:
+        indices: set = set()
+        curr_tallest_tree: int = tree_line[0]
+        for i, tree in enumerate(tree_line[1:], 1):
+            if tree > curr_tallest_tree:
+                curr_tallest_tree = tree
+                indices.add(i)
+            if tree == tallest_tree:
+                break
+        return indices
+
+    def get_visible_tree_indices_backward(self, tree_line: list, tallest_tree: int = 9) -> set:
+        indices: set = set()
+        last_index = len(tree_line) - 1
+        curr_tallest_tree: int = tree_line[last_index]
+        for i in range(last_index - 1, 0, -1):
+            tree = tree_line[i]
+            if tree > curr_tallest_tree:
+                curr_tallest_tree = tree
+                indices.add(i + 1)
+            if tree == tallest_tree:
+                break
+        return indices
 
     def solve_part2(self):
         pass
