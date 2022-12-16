ebf2d185dc424c580fffbd10147a581119cf537e
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-16 18:02:33 -0500

    y2022d9p1: Create Day 9

---
+++ src/puzzles/2022/Day 9/test_rope_bridge.py
@@ -0,0 +1,15 @@
+import pytest
+import rope_bridge
+
+
+@pytest.fixture
+def script():
+    return rope_bridge.RopeBridge("sample.in")
+
+
+def test_sample_input_part1(script):
+    pass
+
+
+def test_sample_input_part2(script):
+    pass
