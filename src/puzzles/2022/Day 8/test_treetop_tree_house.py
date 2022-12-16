abaf755255178c3b3dc60d6293cd78b3afcbf014
Alex Warren <alexander.ch.warren@gmail.com>
2022-12-15 22:30:01 -0500

    y2022d8p1: Create Day 8

---
+++ src/puzzles/2022/Day 8/test_treetop_tree_house.py
@@ -0,0 +1,16 @@
+import pytest
+
+import treetop_tree_house
+
+
+@pytest.fixture
+def script():
+    return treetop_tree_house.TreetopTreeHouse("sample.in")
+
+
+def test_sample_input_part1(script):
+    pass
+
+
+def test_sample_input_part2(script):
+    pass
