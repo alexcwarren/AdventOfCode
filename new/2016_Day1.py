8ba552a88aa74f97c16bb43378496d524529dd33
Alex Warren <alexander.ch.warren@gmail.com>
2023-05-03 09:08:26 -0400

    y2016d1p1: Create "new" directory w/ Day module; Create 2016_Day1

---
+++ new/2016_Day1.py
@@ -0,0 +1,13 @@
+from Day import Day
+
+
+def solve1():
+    pass
+
+
+def solve2():
+    pass
+
+
+if __name__ == "__main__":
+    day1 = Day(__file__)
---
+++ new/Day.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+class Day:
+    def __init__(self, filename: str):
+        self.__filepath: Path = Path(filename)
+        self.__filename: str = self.__filepath.name.split(".")[0]
+        self.input_path: Path = self.__filepath.parent.joinpath(f"input/{self.__filename}.in")
+
+
+if __name__ == "__main__":
+    print("Testing Day module...")
+    day = Day(__file__)
+    assert day.input_path
+    print("SUCCESS\n")
